f90d83d13f208c83130076192eaa4e3281e93903
dagremont@mail.ru <dagremont@mail.ru>
2021-12-19 13:02:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();
+
+            GeomProgression geom = new GeomProgression();
+
+            Console.ReadLine();
+
+        }
+    }
+
+    public interface ISeries
+    {
+        void setStart(int x);
+
+        int getNext();
+        void reset();
+    }
+
+
+    public class ArithProgression : ISeries
+    {
+
+        public void setStart(int x)
+        {
+            return;
+
+        }
+        public int getNext()
+        {
+
+            return 1;
+        }
+        public void reset()
+        {
+
+        }
+        public ArithProgression()
+        {
+
+            int i, Sn, y;
+            y = 80;
+            Sn = 1;
+            for (i = 1; i <= y; i++)
+            {
+                Sn += i;
+                Console.WriteLine(Sn);
+            }
+
+
+        }
+    }
+    public class GeomProgression : ISeries
+    {
+        public void setStart(int x)
+        {
+            return;
+        }
+        public int getNext()
+        {
+            return 1;
+        }
+
+        public void reset()
+        {
+            throw new NotImplementedException();
+        }
+        public GeomProgression()
+        {
+
+            int i, Sn, y;
+            y = 80;
+            Sn = 1;
+            for (i = 1; i <= y; i++)
+            {
+                Sn *= i;
+                Console.WriteLine(Sn);
+            }
+
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp_15")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp_15")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7f7c10d5-6ef7-4633-bbed-3e227a586bbd")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
